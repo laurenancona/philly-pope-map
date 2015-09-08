8dc2ac9922476b50fadc4eb2f98f6e4f8db5493b
Lauren Ancona <lauren.ancona@phila.gov>
2015-09-08 16:57:49 -0400

    style updating, geojson sources

--- js/map.js
+++ js/map.js
@@ -1,30 +1,59 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoibGF1cmVuYW5jb25hIiwiYSI6IjYxNGUxN2ExMmQzZWVkMThhZjY2MGE0YmQxZWZlN2Q2In0.18vQmCC7jmOvuHNnDh8Ybw';
+// Construct a bounding box
+var southWest = L.latLng(39.864439, -75.387541),
+  northEast = L.latLng(40.156325, -74.883544),
+  bounds = L.latLngBounds(southWest, northEast);
 
-	    L.mapbox.accessToken = 'pk.eyJ1IjoibGF1cmVuYW5jb25hIiwiYSI6IjYxNGUxN2ExMmQzZWVkMThhZjY2MGE0YmQxZWZlN2Q2In0.18vQmCC7jmOvuHNnDh8Ybw';
-		  	// Construct a bounding box
-				var southWest = L.latLng(39.864439, -75.387541),
-		   			northEast = L.latLng(40.156325, -74.883544),
-				    bounds = L.latLngBounds(southWest, northEast);
+var map = L.mapbox.map('map', 'laurenancona.2ff8c154', { // Popemap polygons baselayer
+  // set that bounding box as maxBounds to restrict moving the map (http://leafletjs.com/reference.html#map-maxbounds)
+  maxBounds: bounds,
+  maxZoom: 17,
+  minZoom: 12
+})
 
-	     	var map = L.mapbox.map('map','laurenancona.2ff8c154', { // Popemap polygons baselayer
-	     		// set that bounding box as maxBounds to restrict moving the map (http://leafletjs.com/reference.html#map-maxbounds)
-			    	maxBounds: bounds,
-			    	maxZoom: 17,
-			    	minZoom: 12
-				});
+.addControl(L.mapbox.geocoderControl('mapbox.places', {
+  autocomplete: true
+}));
 
-	     	L.control.locate().addTo(map);
 
-				map.fitBounds(bounds); // zoom/snap the map to that bounding box
+// Locate user
+L.control.locate().addTo(map);
 
-				// Here be our data layers
-            var featureLayer = L.mapbox.featureLayer().addTo(map);                    featureLayer.loadURL('https://gist.githubusercontent.com/laurenancona/12875fed89f70140c706/raw/93d3d0f7e207d6baa8a1b46da196f48505854b0e/screens.geojson');
+map.fitBounds(bounds); // zoom/snap the map to that bounding box
 
-		    var points = L.mapbox.tileLayer('laurenancona.4ae9c933').addTo(map);
-				   // blocks = L.mapbox.tileLayer('laurenancona.fc7871b8'),
-				  //  legend = map.legendControl.addLegend(document.getElementById('legend').innerHTML); // add legend		
-				      //lots			= L.mapbox.featureLayer('').addTo(map);
-	    
-	    // Layer control freak
+//// Here be our data layers
+var screens = L.mapbox.featureLayer().addTo(map);
+screens.loadURL('https://gist.githubusercontent.com/laurenancona/12875fed89f70140c706/raw/93d3d0f7e207d6baa8a1b46da196f48505854b0e/screens.geojson');
+
+// Listen for individual marker clicks.
+screens.on('click',function(e) {
+    // Force the popup closed.
+    e.layer.closePopup();
+
+    var feature = e.layer.feature;
+    var content = '<div><strong>' + feature.properties.name + '</strong>' +
+                  '<p>' + feature.properties.type + '</p></div>';
+
+    info.innerHTML = content;
+});
+
+// Clear the tooltip when map is clicked.
+map.on('move', empty);
+
+// Trigger empty contents when the script
+// has loaded on the page.
+empty();
+
+function empty() {
+  info.innerHTML = '<div><strong>Click a marker</strong></div>';
+  }
+
+var points = L.mapbox.tileLayer('laurenancona.4ae9c933').addTo(map);
+// blocks = L.mapbox.tileLayer('laurenancona.fc7871b8'),
+//  legend = map.legendControl.addLegend(document.getElementById('legend').innerHTML); // add legend		
+//lots			= L.mapbox.featureLayer('').addTo(map);
+
+// Layer control freak
 //	    L.control.layers({
 //		  	// 'Parking Dark': L.mapbox.tileLayer('laurenancona.mgb93lh3').addTo(map),
 //		  	// 'Parking Light': L.mapbox.tileLayer('laurenancona.fc7871b8')
@@ -44,25 +73,25 @@
 //				var blocksControl = L.mapbox.gridControl(blocksGrid	).addTo(map); //,
 //				 	// lotsControl  			=   L.mapbox.gridControl('laurenancona.fc7871b8').addTo(map);
 
-				var hash = L.hash(map); // append (z)/(x)/(y) to URL for deep linking to locations
+var hash = L.hash(map); // append (z)/(x)/(y) to URL for deep linking to locations
 
-				map.setView([39.9547, -75.1695], 14);
+map.setView([39.9547, -75.1695], 14);
 
-			// ht @konklone for console.log-fication example
-			// for sad, sad IE:
-			  if (window._ie) {
-					console.log("Hey there."); 
-					console.log("If you're into this kind of thing and want to help out, let me know."); 
-					console.log("http://github.com/laurenancona or @laurenancona"); 
-			  }
+// ht @konklone for console.log-fication example
+// for sad, sad IE:
+if (window._ie) {
+  console.log("Hey there.");
+  console.log("If you're into this kind of thing and want to help out, let me know.");
+  console.log("http://github.com/laurenancona or @laurenancona");
+}
 
-  			// otherwise, style it up:
-			  else {
-			    var styles = {
-			      medium: "font-size: 10pt, font-weight: bold;color: #1B3B56",
-			      medium_link: "font-size: 10pt; font-weight: bold; color: #027ea4",
-			    };
-			    console.log("%cHey there", styles.medium); 
-					console.log("%cIf you're into this kind of thing and want to help out, let me know.", styles.medium); 
-					console.log("%chttp://github.com/laurenancona or @laurenancona", styles.medium); 
-			  }
+// otherwise, style it up:
+else {
+  var styles = {
+    medium: "font-size: 10pt, font-weight: bold;color: #1B3B56",
+    medium_link: "font-size: 10pt; font-weight: bold; color: #027ea4",
+  };
+  console.log("%cHey there", styles.medium);
+  console.log("%cIf you're into this kind of thing and want to help out, let me know.", styles.medium);
+  console.log("%chttp://github.com/laurenancona or @laurenancona", styles.medium);
+}
